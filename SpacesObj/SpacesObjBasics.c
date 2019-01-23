a5e4217197c013caacecd18e4d7f1cd46989ca2c
titosilva <jt.mat@hotmail.com>
2019-01-23 13:54:20 -0200

    Updating SpacesObjBasics.c

--- SpacesObj/SpacesObjBasics.c
+++ SpacesObj/SpacesObjBasics.c
@@ -27,11 +27,11 @@ int deleteObject(DimObject *obj){
                 for(int i=0; i<obj->pointquantity; i++){
                     free(obj->points[i]);
                     free(obj->adjacency[i]);
-                    printf("%d\n", i);
                 }
                 free(obj->points);
                 free(obj->adjacency);
                 obj->deleted = 1;
+                obj->pointquantity = 0;
                 return 0;
             }else{
                 return -1;
@@ -47,7 +47,7 @@ int deleteObject(DimObject *obj){
 DimObject copyObject(DimObject *obj){
     if(!obj->deleted){
         DimObject object;
-        if(obj->points==NULL && obj->adjacency==NULL){
+        if(obj->points==NULL || obj->adjacency==NULL){
             object.adjacency = NULL;
             object.points = NULL;
             object.points = 0;
@@ -68,60 +68,45 @@ DimObject copyObject(DimObject *obj){
 }
 
 int addPoint(DimObject *obj, float *point){
-    if(!obj->deleted && !obj->error){
-        if(obj->adjacency==NULL && obj->points==NULL && obj->pointquantity==0){
-            obj->points = (float**) calloc(1, sizeof(float*));
-            obj->adjacency = (int**) calloc(1, sizeof(int*));
-            if(obj->adjacency!=NULL && obj->points!=NULL){
-                obj->points[0] = (float*) calloc(obj->dimension, sizeof(float));
-                obj->adjacency[0] = (int*) calloc(1, sizeof(int));
-                if(obj->points[0]!=NULL && obj->adjacency[0]!=NULL){
-                    for(int i=0; i<obj->dimension; i++){
-                        obj->points[0][i] = point[i];
+    if(!obj->error && !obj->deleted){
+        float **newpointmat = (float**) calloc(obj->pointquantity+1, sizeof(float*));
+        int **newadjmat = (int**) calloc(obj->pointquantity+1, sizeof(int*));
+        if(newadjmat != NULL && newpointmat != NULL){
+            for(int i=0; i<obj->pointquantity+1 && obj->error==0; i++){
+                newpointmat[i] = (float*) calloc(obj->dimension, sizeof(float));
+                newadjmat[i] = (int*) calloc(obj->pointquantity+1, sizeof(int));
+                if(newadjmat[i] != NULL && newpointmat[i] != NULL){
+                    for(int j=0; j<obj->dimension; j++){
+                        if(i==obj->pointquantity){
+                            newpointmat[i][j] = point[j];
+                        }else{
+                            newpointmat[i][j] = obj->points[i][j];
+                        }
                     }
-                    obj->pointquantity++;
-                }else{
-                    obj->error = DIMOBJ_ERROR_ALLOC | 0xA2000000;
-                }
-            }else{
-                obj->error = DIMOBJ_ERROR_ALLOC | 0xA1000000;
-            }
-        }else{
-            // New adjacency matrix
-            printf("K: %d\n", obj->pointquantity);
-            obj->adjacency = (int**) realloc(obj->adjacency, obj->pointquantity+1);
-            if(obj->adjacency==NULL){
-                obj->error = DIMOBJ_ERROR_ALLOC | 0xA3000000;
-            }else{
-                obj->adjacency[obj->pointquantity] = (int*) calloc(obj->pointquantity, sizeof(int));
-                if(obj->adjacency==NULL){
-                    obj->error = DIMOBJ_ERROR_ALLOC | 0xA4000000;
-                }else{
-                    for(int i=0; i<obj->pointquantity+1 && obj->error==0; i++){
-                        obj->adjacency[i] = (int*) realloc(obj->adjacency[i], obj->pointquantity+1);
-                        if(obj->adjacency[i]==NULL) obj->error = DIMOBJ_ERROR_ALLOC | 0xA5000000;
-                        else obj->adjacency[i][obj->pointquantity] = 0;
+                    for(int j=0; j<obj->pointquantity; j++){
+                        if(i!=obj->pointquantity){
+                            newadjmat[i][j] = obj->adjacency[i][j];
+                        }
                     }
+                }else{
+                    obj->error = DIMOBJ_ERROR_ALLOC | 0xA1000000;
                 }
             }
 
             if(!obj->error){
-                // New points matrix
-                obj->points = (float**) realloc(obj->points, obj->pointquantity+1);
-                if(obj->points!=NULL){
-                    obj->points[obj->pointquantity] = (float*) calloc(obj->dimension, sizeof(float));
-                    if(obj->points[obj->pointquantity]==NULL){
-                        obj->error = DIMOBJ_ERROR_ALLOC | 0xA7000000;
-                    }else{
-                        for(int i=0; i<obj->dimension; i++){
-                            obj->points[obj->pointquantity][i] = point[i];
-                        }
-                        obj->pointquantity++;
-                    }
-                }else{
-                    obj->error = DIMOBJ_ERROR_ALLOC | 0xA6000000;
+                for(int i=0; i<obj->pointquantity; i++){
+                    free(obj->points[i]);
+                    free(obj->adjacency[i]);
                 }
+                free(obj->points);
+                free(obj->adjacency);
+
+                obj->adjacency = newadjmat;
+                obj->points = newpointmat;
+                obj->pointquantity++;
             }
+        }else{
+            obj->error = DIMOBJ_ERROR_ALLOC | 0xA0000000;
         }
     }else{
         if(obj->deleted){
@@ -129,7 +114,6 @@ int addPoint(DimObject *obj, float *point){
         }
     }
 
-    printf("%d\n", obj->pointquantity);
     return obj->error;
 }
 
