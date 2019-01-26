c6eb3afbe776778bafaa09c8ccd647954a9172be
titosilva <jt.mat@hotmail.com>
2019-01-26 09:22:03 -0200

    Adding SpaceDim.h file

---
+++ SpaceDim.h
@@ -0,0 +1,8 @@
+#ifndef SPACEDIM_SPACEDIM_H
+#define SPACEDIM_SPACEDIM_H
+
+#include "./SpacesObj/SpacesObjBasics.h"
+#include "./SpacesObj/SpacesObjTransform/SpacesObjTransform.h"
+#include "./SpacesObj/SpacesObjForSFML/SpacesObjForSFML.h"
+
+#endif
